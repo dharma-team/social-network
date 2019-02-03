46ff1a400b2cd2cf99071a1c0d793195a4cf6a8a
akvila <sanek.ko4nar@yandex.ru>
2019-02-03 21:26:35 +0300

    Start Files

---
+++ index.php
@@ -0,0 +1,8 @@
+<html>
+<head>
+	<title>Welcome Social Network</title>
+</head>
+<body>
+	<h1>Hello Friend!</h1>
+</body>
+</html>
---
+++ register.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+	<title>Welcome Social Network</title>
+</head>
+<body>
+	<form action="register.php">
+		<input type="text" name="reg_fname" placeholder="First Name" required>
+
+		<input type="text" name="reg_lname" placeholder="Last Name" required>
+
+		<input type="email" name="reg_fname" placeholder="Email" required>
+
+		<input type="email" name="reg_email" placeholder="Confim Email" required>
+
+		<input type="password" name="reg_password" placeholder="Password" required>
+
+		<input type="password" name="reg_password2" placeholder="Confim Password" required>
+
+		<input type="submit" name="register_button" value="Register">
+	</form>
+</body>
+</html>
