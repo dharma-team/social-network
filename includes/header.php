c805a3727aab251d5cc7abcb3a91db5dea2291f6
akvila <sanek.ko4nar@yandex.ru>
2019-02-06 19:53:57 +0300

    Fix Bug

--- includes/header.php
+++ includes/header.php
@@ -1,7 +1,7 @@
 <?php
 include_once 'config/config.php';
 
-if (isset($_SESSION['username'])), {
+if (isset($_SESSION['username'])) {
 	$userLoggedIn = $_SESSION['username'];
 	$user_details_query = mysqli_query($con, "SELECT * FROM users WHERE username='$userLoggedIn'");
 	$user = mysqli_fetch_array($user_details_query);
